d762292aa46689a9e109ce0bb7323cf5e4a4212b
tblanshard <43909036+tblanshard@users.noreply.github.com>
2020-09-15 20:54:25 +0100

    Update GoodPairs.py

--- LeetCode/GoodPairs.py
+++ LeetCode/GoodPairs.py
@@ -7,8 +7,15 @@ class Solution:
    #             if (nums[i] == nums[j] and i < j):
     #                count+=1
     
-        for i,x in enumerate(nums):
+        #for i,x in enumerate(nums):
+        for i in range(len(nums)):
             for j in range(i, len(nums)):
-                if (x == nums[j] and i < j):
+                #if (x == nums[j] and i < j):
+                if (nums[i] == nums[j] and i < j):
                     count+=1
         return count
+
+    
+    #current code stats:
+    #Runtime: 28 ms, faster than 91.27% of Python3 online submissions for Number of Good Pairs.
+    #Memory Usage: 13.9 MB, less than 41.71% of Python3 online submissions for Number of Good Pairs.
