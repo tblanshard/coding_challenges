af2ac4cad2b05f7d9dda7b5d0165577bfee25560
tblanshard <43909036+tblanshard@users.noreply.github.com>
2020-09-16 18:07:10 +0100

    Update SmallerThanCurrent.py

--- LeetCode/SmallerThanCurrent.py
+++ LeetCode/SmallerThanCurrent.py
@@ -1,15 +1,36 @@
 class Solution:
     
-    def filterHelper(self, number, greaterThan):
-        return (number < greaterThan)
+    #def filterHelper(self, number, greaterThan):
+    #    return (number < greaterThan)
     
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        answer = []
-        for i,n in enumerate(nums):
-            copy = nums[:]
-            copy.pop(i)
-            answer.append(len(list(filter(lambda number: self.filterHelper(number,n), copy))))
-        return answer
+        #ATTEMPT 1
+        #answer = []
+        #for i,n in enumerate(nums):
+        #    copy = nums[:]
+        #    copy.pop(i)
+        #    answer.append(len(list(filter(lambda number: self.filterHelper(number,n), copy))))
+        #return answer
+    
+        #ATTEMPT 2
+        #result = [];
+        #for number in nums:
+        #    result.append([n < number for n in nums].count(True))
+        #return result
         
- #good space complexity
- #poor time complexity - needs work!
+        #ATTEMPT 3
+        countLessThan = {}
+        result = []
+        sortedNums = sorted(nums)
+        unique = sorted(set(nums), reverse=True)
+        for i,number in enumerate(unique):
+            countLessThan[number] = len(sortedNums[0:sortedNums.index(number)])
+        for n in nums:
+            result.append(countLessThan[n])
+        return result
+    
+    # current solution stats
+    # Runtime: 56 ms, faster than 84.77% of Python3 online submissions for How Many Numbers Are Smaller Than the Current Number.
+    # Memory Usage: 13.9 MB, less than 38.44% of Python3 online submissions for How Many Numbers Are Smaller Than the Current Number.
+    
+    # need to improve space complexity!
